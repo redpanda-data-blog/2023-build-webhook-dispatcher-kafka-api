6274d2cd1ed7830ec3d87b487357ab22ee425fe9
Nick Abbene <njabbene@gmail.com>
2023-03-19 18:33:11 -0400

    update external api to receive webhook

--- external/start/routes.ts
+++ external/start/routes.ts
@@ -20,6 +20,7 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
+Route.post('/help-desk/webhook', async ({ request }) => {
+  console.log(`Received webhook with body: ${JSON.stringify(request.body())}`)
+  return { success: 'true' }
 })
